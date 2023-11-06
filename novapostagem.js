a50b48113bb78c4ca177ce601245ab1c0a4e2825
Ryanfs-ng <144951184+Ryanfs-ng@users.noreply.github.com>
2023-11-06 18:13:10 -0300

    Nova postagem update

---
+++ novapostagem.js
@@ -0,0 +1,5 @@
+document.getElementById("botnovapostagem").addEventListener("click", function() {
+  // Redirecionar para a próxima página
+  window.location.href = "http://127.0.0.1:5500/Forum2-main/novapostagem.html";
+});
+
