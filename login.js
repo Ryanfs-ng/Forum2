f02abc0dbd0b51b79be438179ed3a10926607320
Ryanfs-ng <ryanfsilva6@gmail.com>
2023-11-13 00:05:31 -0300

    validação login

--- login.js
+++ login.js
@@ -9,3 +9,41 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+// Validação de login 
+
+const usuarios = [
+    {
+        login: 'ApenasUmUsuário',
+        pass: '1234'
+    },
+    {
+        login: 'ProfWeldes',
+        pass: '1234'
+    }
+]
+
+let botao = document.getElementById('btnLogar')
+
+botao.addEventListener('click', function logar(){
+    let getUsuario = document.getElementById('usuario').value
+    let getSenha = document.getElementById('senha-user').value
+    let validaLogin = false
+
+    for(let i in usuarios){
+
+        if(getUsuario == usuarios[i].login && getSenha == usuarios[i].pass){
+            validaLogin = true
+            break;
+        }else{
+            validaLogin = false
+        }
+    }
+
+    if(validaLogin == true){
+        alert('ok')
+    } else {
+        alert ('erro')
+    }
+})
+
